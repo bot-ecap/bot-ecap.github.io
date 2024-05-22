8901bd72caad0bf82d409dc32c85f1bee178ed58
Kevin Liu <javaandroidc@qq.com>
2024-05-22 18:48:39 +0800

    update file by keivn

--- pushfile_copy.cmd
+++ pushfile_copy.cmd
@@ -1,6 +1,6 @@
 
 d:
-cd D:\sourcecode\ecap-NN\ChatGPT\sourcecode\2024\myGithub\NLG-Chat-WikiCode\_book
-xcopy  /E /Y * "D:\sourcecode\ecap-NN\ChatGPT\sourcecode\2024\myGithub\6LLM.github.io" 
-cd D:\sourcecode\ecap-NN\ChatGPT\sourcecode\2024\myGithub\6LLM.github.io
+cd D:\sourcecode\ecap-NN\ChatGPT\sourcecode\2024\myGithub\wikiecap\botwiki\_book
+xcopy  /E /Y * "D:\sourcecode\ecap-NN\ChatGPT\sourcecode\2024\myGithub\github.io.ecap\bot-ecap.github.io" 
+cd D:\sourcecode\ecap-NN\ChatGPT\sourcecode\2024\myGithub\github.io.ecap\bot-ecap.github.io
 git add . && git commit -m"update file by keivn" && git push
