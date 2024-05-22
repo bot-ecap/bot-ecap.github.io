9b2543e63536893ffb37e6552b409b2da73b44ab
Kevin Liu <javaandroidc@qq.com>
2024-05-22 20:10:33 +0800

    init

--- PushFile.cmd
+++ PushFile.cmd
@@ -1,6 +1,6 @@
 
 d:
-cd D:\sourcecode\ecap-NN\ChatGPT\sourcecode\2024\myGithub\wikiecap\botwiki
+cd D:\sourcecode\ecap-NN\ChatGPT\sourcecode\2024\myGithub\wikiecap 
 git add . && git commit -m"update file by keivn" && git push
 gitbook build && start pushfile_copy.cmd
 
